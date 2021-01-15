7c5d8b6e7b423790c1ab8a63837cbf6ffe76c033
theresale09 <phnguyen0912@gmail.com>
2021-01-15 13:56:36 -0600

    final commit hopefully

--- contactTracing.pl
+++ contactTracing.pl
@@ -1,4 +1,79 @@
-#include 'knowledgeBase.pl'.
+
+%UNIX TIMESTAMP CONVERTER: https://www.unixtimestamp.com/index.php
+
+%DEFINE CONSTANTS
+max_contact_time(900). %900 secs = 15mins
+
+%START ------ SAMPLE KNOWLEDGE BASE ---------------------------------------------------------
+%List of infected students and time of covid contraction
+infected(matt, 1610657119). %01/14/2021 @ 8:45pm (UTC) check locations between 01/01/2021 - 01/14/2021
+infected(jack, 1610280000). %01/10/2021 @ 12:00pm (UTC)
+
+
+% ?- infected(X, _).
+
+%List of in-person events that the infected student went to in the last 2 weeks
+%infected_event(Name_Of_Event, StartT, EndT) %StartT and EndT denotes that start and end time of an event
+infected_event(hackathon, ecsw, 1609848000, 1609934400).   %01/05/2021 @ 12:00pm (UTC)- 01/06/2021 @ 12:00pm (UTC)
+infected_event(luncheon, jsom, 1610280000, 1610283600).    %01/10/2021 @ 12:00pm (UTC) - 01/10/2021 @ 1:00pm (UTC)
+infected_event(career_fair, visitor_center, 1610013600, 1610031600). %01/07/2021 @ 10:00am (UTC) - 01/07/2021 @ 3:00pm (UTC)
+infected_event(career_fair, science_building, 1610013600, 1610031600). %01/07/2021 @ 10:00am (UTC) - 01/07/2021 @ 3:00pm (UTC)
+
+%List of people that attended all events
+%---attended_event(Name, Event_Name, Building) 
+attended_event(matt, hackathon).
+attended_event(matt, luncheon).
+attended_event(matt, career_fair).
+attended_event(nguyen, hackathon).
+attended_event(jack, hackathon).
+attended_event(anusha, hackathon).
+attended_event(jack, career_fair).
+
+
+%list of potentially infected classmates (TO DO: replace with actual courses)
+person_in_class(matt, cs4337).
+person_in_class(jack, cs3354).
+person_in_class(nguyen, cs4337).
+person_in_class(matt, cs2337).
+person_in_class(eve, cs1333).
+person_in_class(anusha, cs2337).
+
+%Commuter or Roommate
+commuter(troy).
+commuter(tea).
+commuter(eve).
+commuter(jade).
+
+roommates1(jack, matt).
+roommates1(jack, nguyen).
+roommates1(matt, anusha).
+roommates1(anusha, jerry).
+
+%TODO - Add more data
+%List of each student highest temperature (Fahrenheit) in the last 2 weeks 
+student_temp(matt, 110).
+student_temp(jack, 98).
+student_temp(anusha, 98).
+student_temp(nguyen, 110).
+student_temp(tom, 110).
+student_temp(jerry, 110).
+
+%medical history 
+high_cholesterol([nguyen, jack, sam]).
+heart_conditions([anusha, sam, matt, terry]).
+diabetes([tom, jerry, maria]).
+cancer([eve, ai, phuong, theresa]).
+
+% person_in_buildings(Person, BuildingID, StartT, EndT). StartT & EndT in unix timestamps 
+% this list is updated frequently based on sign-ins and sign-outs based on card swipe
+person_in_buildings(matt, science_building, 1610651386, 1610652386).
+person_in_buildings(jack, ecss, 1610651486, 1610652486).
+person_in_buildings(nguyen, ecsw, 1610651386, 1610652386).
+person_in_buildings(tea, jsom, 1610651486, 1610652486).
+person_in_buildings(anusha, ecss, 1610651386, 1610652386).
+person_in_buildings(eve, science_building, 1610651486, 1610652486).
+
+%END ------ SAMPLE KNOWLEDGE BASE ---------------------------------------------------------
 
 %START ------FUNCTION HELPER ---------------------------------
 
@@ -43,6 +118,26 @@ append([], L, L).
 append([H|T], L2, [H|L3]) :- append(T,L2,L3).
 append(H, L, [H|L]).
 
+%remove duplicates in list of lists
+remove_dupl(InL, OutL):- remove_dupl(InL, [], OutL1),remove_empty(OutL1,OutL).
+remove_dupl([],_,[]).
+remove_dupl([H|T],L,[[H1]|T2]):-
+    H=[H1], not member(H1,L), 
+    remove_dupl(T,[H1|L],T2).
+remove_dupl([H|T],L,[[H1|T2]|T3]):- 
+    H=[H1|T1], not member(H1,L), 
+    remove_dupl([T1|T],[H1|L],[T2|T3]).
+remove_dupl([H|T],L,T2):- 
+    H=[H1|T1], member(H1,L), 
+    remove_dupl([T1|T],L,T2).
+remove_dupl([H|T],L,[[]|T2]):- 
+    H=[H1], member(H1,L), 
+    remove_dupl(T,L,T2).
+
+remove_empty([],[]).
+remove_empty([[]|T],T1):-remove_empty(T,T1).
+remove_empty([[H|T]|T1],[[H|T]|T2]):-remove_empty(T1,T2).
+
 %END------FUNCTION HELPER ---------------------------------
 
 %main program: generate a list of people who could have been in contact with the infected person in the past 2 weeks.
@@ -60,10 +155,20 @@ critical_health_status(Potential):-
     at_risk(Potential).
 
 % close contact conditions
-close_contact(Infected, PersonX):- infected(Infected, _), time_greater_than_fifteen(Infected, PersonX). 
-close_contact(Infected, PersonX):- infected(Infected, _), in_same_class(Infected, PersonX). 
-close_contact(Infected, PersonX):- infected(Infected, _), not commuter(Infected),  not commuter(PersonX), roommates(Infected, PersonX).
-close_contact(Infected, PersonX):- infected(Infected, _), went_to_infected_event(Infected, PersonX, Event).
+close_contact(Infected, PersonX):- 
+    infected(Infected, _), 
+    time_greater_than_fifteen(Infected, PersonX). 
+close_contact(Infected, PersonX):- 
+    infected(Infected, _), 
+    in_same_class(Infected, PersonX). 
+close_contact(Infected, PersonX):- 
+    infected(Infected, _), 
+    not commuter(Infected),  
+    not commuter(PersonX), 
+    roommates(Infected, PersonX).
+close_contact(Infected, PersonX):- 
+    infected(Infected, _), 
+    went_to_infected_event(Infected, PersonX, Event).
 
 %get roommates of infected students 
 roommates2(X, Y) :- roommates1(X, Y).
@@ -127,6 +232,10 @@ potential_infected_students(Infected, Result) :-
     infected(Infected, _),
     findall(X, potential_infected_student(Infected, X), Xs), 
     set(Xs, Result).
+get_all_potential_infected_students(Result) :- 
+    findall(X, potential_infected_students(Infected, X), Xs), 
+    set(Xs, Result1),
+    remove_dupl(Result1, Result).
 
 %conditions for critical_potential_infected_student to hold:
     % 1) student Potential is not already infected
@@ -141,6 +250,10 @@ critical_potential_infected_students(Infected, Result) :-
     infected(Infected, _),
     findall(X, critical_potential_infected_student(Infected, X), Xs), 
     set(Xs, Result).
+get_all_critical_students(Result) :- 
+    findall(X, critical_potential_infected_students(Infected, X), Xs), 
+    set(Xs, Result1),
+    remove_dupl(Result1, Result).
 
 %check if a potentially infected student went to an infected event
 went_to_infected_event(Potential, Event) :- 
